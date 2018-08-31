52b9132163845dedfcd8314a4330165c863c7878
Kayhan <kdehghan@sfu.ca>
2018-08-30 19:11:29 -0700

    Modify news module

--- MMM-dshop-news/MMM-dshop-news.js
+++ MMM-dshop-news/MMM-dshop-news.js
@@ -8,44 +8,42 @@ Module.register("MMM-dshop-news", {
 		isLoading: true,
 		contentIndex: 0,
 		lastAction: "",
-		currentFormType: "",
-		forms: {}
+		forms: []
 	},
-	
+
 	start() {
 		Log.log(this.name + " is started!");
-		// TODO: navigate between types from user's input
-		let formType = this.config.formTypes[0];
-		let url = this.config.url + "forms" + "/" + formType
-		this.callUrl(url, formType);
+		let url = this.config.url + "forms";
+		this.callUrl(url);
 	},
-	
+
 	loaded(callback) {
 		Log.log(this.name + " is loaded!");
 		callback();
 	},
-	
+
 	getDom() {
 		var content = document.createElement("div");
-    if(this.config.isLoading) {
+		if (this.config.isLoading) {
 			content = this.createSpinner();
 		} else {
-			let currForm = this.config.forms[this.config.currentFormType][0];
+			let currForm = this.config.forms[this.config.contentIndex];
 			content = this.createTemplate(currForm.title, currForm.date, currForm.location, currForm.description, currForm.imagePaths[0]);
 		}
 		return content;
 	},
-	
+
 	createSpinner() {
 		var spinner = document.createElement("div");
 		spinner.id = "spinner";
 		return spinner;
 	},
-	
+
 	createTemplate(title, date, location, description, imagePath) {
 		// TODO: carousel with multiple images
 		let contentDiv = document.createElement("div");
 		contentDiv.id = "news-content";
+		contentDiv.className = this.config.lastAction;
 		contentDiv.innerHTML = `
 		<div class="mdl-grid">
 			<div class="mdl-cell mdl-cell--1-col">
@@ -74,22 +72,45 @@ Module.register("MMM-dshop-news", {
 		return contentDiv;
 	},
 
+	notificationReceived(notification, payload, sender) {
+		if (sender && sender.name === "MMM-Flick" && notification === "SENSOR_SWIPED") {
+			switch (payload.action) {
+				case 'right':
+				if (this.config.contentIndex < this.config.forms.length - 1) {
+					this.config.contentIndex++;
+					this.swipeHelper(payload.action);
+				}
+				break;
+				case 'left':
+				if (this.config.contentIndex > 0) {
+					this.config.contentIndex--;
+					this.swipeHelper(payload.action);
+				}
+				break;
+			}
+		}
+	},
+
+	swipeHelper(action) {
+		this.config.lastAction = action;
+		this.updateDom();
+	},
+
 	getStyles() {
 		return [this.file("/css/main.css"), this.file("/css/material.min.css")];
 	},
 
-	callUrl(url, type) {
+	callUrl(url) {
 		fetch(url)
-    .then(response => response.json())
-    .then(json => { 
-			this.config.forms[type] = json;
-			this.config.currentFormType = type;
-			this.config.isLoading = false;
-			this.updateDom(); 
-		})
-    .catch(err => {
-        console.log("Fetch Error :-S", err);
-		});
+			.then(response => response.json())
+			.then(json => {
+				this.config.forms = json;
+				this.config.isLoading = false;
+				this.updateDom();
+			})
+			.catch(err => {
+				console.log("Fetch Error :-S", err);
+			});
 	},
 
 });
